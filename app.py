53d1ae93984579b3bcc4fe0f4ec88344592e01d9
Плохотнюк Артем <aplokhotnyuk@diasoft.ru>
2025-04-12 20:45:34 +0300

    bye

--- app.py
+++ app.py
@@ -2,11 +2,8 @@ from PyQt6.QtWidgets import QApplication
 from main_win import MainWindow
 import sys
 
-def close():
-    print("Closing...")
-
 app = QApplication(sys.argv)
 window = MainWindow()
 window.showMaximized()
-app.destroyed.connect(close)
 app.exec()
+print("Bye")
